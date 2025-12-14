6732c45cd979cba7041ea8c2e336a1e573108927
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-14 08:56:40 +0100

    init auth service

---
+++ authenticationservice/src/test/java/com/mikky/corebanking/authenticationservice/AuthenticationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikky.corebanking.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
