8602501f995dcfc3c25eb4f4a8dfa61d076f3348
mikcheal101 <hirekaanmicheal@gmail.com>
2025-12-17 15:50:49 +0100

    events library initialized

---
+++ events/src/test/java/com/mikky/corebanking/events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikky.corebanking.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
